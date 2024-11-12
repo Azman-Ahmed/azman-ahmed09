5c74cc6fe774484ba6819dc3530b90a7b0e0b2a5
Azman-Ahmed <ahmed.azman.bd0007@gmail.com>
2024-11-12 13:26:30 +0600

    sizing the banner size

--- src/app/_components/HomeComponents/Banner.tsx
+++ src/app/_components/HomeComponents/Banner.tsx
@@ -1,10 +1,11 @@
 "use client";
 import React from "react";
 import BannerLayout from "../layouts/BannerLayout";
-import Typewriter from "typewriter-effect";
+import dynamic from "next/dynamic";
 import azman from "../../../../public/me.png"
 import Image from "next/image";
 
+const Typewriter = dynamic(() => import("typewriter-effect"), { ssr: false }); // Dynamically import without SSR
 
 const Banner = () => {
   return (
