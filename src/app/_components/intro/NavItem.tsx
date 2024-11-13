18171798d488f6718f803da27c49fb81b73c6c03
Azman-Ahmed <ahmed.azman.bd0007@gmail.com>
2024-11-13 13:52:35 +0600

    made the right navbar functional

--- src/app/_components/intro/NavItem.tsx
+++ src/app/_components/intro/NavItem.tsx
@@ -1,22 +1,23 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router';
-import React from 'react'
+// NavItem.tsx
+import Link from "next/link";
+import { usePathname } from "next/navigation"; 
+import React from 'react';
 
-
-const NavItem = ({ NavIcon, NavText, NavRoute, setIsOpen }:{ NavIcon:any, NavText:any, NavRoute:any, setIsOpen:any }) => {
-    const router = useRouter();
-    const className = router.asPath === `${NavRoute}` ? "rounded-xl !text-DeepNightBlack bg-Green font-bold tracking-widest" : '';
+const NavItem = ({ setIsOpen, NavRoute, NavIcon, NavText }: { setIsOpen:any, NavRoute: string, NavIcon: React.ReactNode, NavText: string }) => {
+    const pathname = usePathname(); 
+    const className = pathname === NavRoute 
+        ? "rounded-xl !text-DeepNightBlack bg-Green font-bold tracking-widest" 
+        : '';
 
     return (
         <Link
-            onClick={(e) => setIsOpen(false)}
             href={NavRoute}
-            className={`${className} transition flex items-center px-2 hover:bg-EveningBlack text-SilverGray hover:text-SilverGray rounded-xl  py-1.5 font-semibold space-x-4 text-base`}
+            className={`${className} transition flex items-center px-2 hover:bg-EveningBlack text-SilverGray hover:text-SilverGray rounded-xl py-1.5 font-semibold space-x-4 text-base`}
         >
             {NavIcon}
             <span>{NavText}</span>
         </Link>
-    )
-}
+    );
+};
 
-export default NavItem
+export default NavItem;
