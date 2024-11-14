ceaae9a5ab60918a87a44e801c476a73b3966a04
Azman-Ahmed <ahmed.azman.bd0007@gmail.com>
2024-11-14 11:39:06 +0600

    added tools segment

--- src/app/_components/intro/Tools.tsx
+++ src/app/_components/intro/Tools.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-
+import Badge from './Badges';
+import { TECH_STACK } from '../constants/constants';
 const Tools = () => {
   return (
-    <div>
-      <h1>IDE</h1>
+    <div className='flex flex-col space-y-1 pt-6'>
+      <div className='flex flex-col gap-y-4'>
+        <span className='text-Snow text-xs font-bold'>Tools</span>
+        <div className='flex flex-wrap gap-2'>
+          {TECH_STACK.map((item, index) => (
+            <Badge key={index} title={item} />
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tools
+export default Tools;
