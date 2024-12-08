fca226c1043ec8f81dc058c7660b7ed4a1114ea9
Azman-Ahmed <ahmed.azman.bd0007@gmail.com>
2024-12-08 10:33:30 +0600

    updated the suggestions

--- src/app/_components/ConstantData/CSE1111fall24.tsx
+++ src/app/_components/ConstantData/CSE1111fall24.tsx
@@ -19,10 +19,7 @@ export interface FinalExam {
           "1. Software: Software chart, Definition of Software and different types of software, Definition of Operating System and types of interfaces",
           "2. HTML: A login form using HTML, CSS",
           "3. C++: Basic Arithmetic Operations using C++ code (Summation, Subtractions, Multiplication, Division, Modulus)",
-          "Video tutorial for C++ : 1. https://youtu.be/RKNgEgm_DP8?si=0vGeHwfRAPwA67me ",
-          "                         2. https://youtu.be/fy4hJWctkNc?si=wZwSDsnt9Em2tnNu",
-
-
+          "Video tutorial for C++ : https://youtu.be/RKNgEgm_DP8?si=0vGeHwfRAPwA67me , https://youtu.be/fy4hJWctkNc?si=wZwSDsnt9Em2tnNu",
         ]
       }
     }
